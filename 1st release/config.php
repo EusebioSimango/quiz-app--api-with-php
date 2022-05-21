bc56515bab8f91de6a84435c59c014559e32e7e6
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-21 20:25:17 +0200

    Connectin to database

---
+++ 1st release/config.php
@@ -0,0 +1,10 @@
+<?php
+define('HOST', '127.0.0.1');
+define('USER', 'eusebio');
+define('PASSWORD', '1234');
+define('DATABASE', 'quizApp');
+
+$connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE) or die("Cannot connect to database!");
+
+
+?>
